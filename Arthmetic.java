a9c1fe8ab00de0d9139eb88465ee790a9196d78c
AnanyaB-05 <245123751063@mvsrec.edu.in>
2026-03-23 15:46:08 +0530

    Update Arthmetic.java

--- Arthmetic.java
+++ Arthmetic.java
@@ -1,23 +1,23 @@
-import java.util.Scanner;
-class Arthmetic
-{
-  public static void main(String[] args)
-  {
-     Scanner sc=new Scanner(System.in);
-     int a,b,add,sub,div,mod,mul;
-     System.out.println("a=");
-     a=sc.nextInt();
-     System.out.println("b=");
-     b=sc.nextInt();
-     add=a+b;
-     System.out.println("add="+add);
-     sub=a-b;
-     System.out.println("sub="+sub);
-     div=a/b;
-     System.out.println("div="+div);
-     mod=a%b;
-     System.out.println("mod="+mod);
-     mul=a*b;
-     System.out.println("mul="+mul);
-   }
-}     
+public class Arthmetic {
+    public static void main(String[] args) {
+
+        // Check if two inputs are provided
+        if (args.length < 2) {
+            System.out.println("Please provide two numbers");
+            return;
+        }
+
+        // Convert arguments to integers
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        // Perform operations
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+    }
+}
